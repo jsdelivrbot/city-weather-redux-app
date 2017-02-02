cf7d8690943bdcfd529be296bd3e85f9f081ec2b
Jonathan Lam <jonathanlam@Jonathans-MacBook.local>
2017-02-01 21:46:41 -0800

    setting up search bar. adding call back function to search bar.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -9,10 +9,32 @@ import React, { Component } from 'react';
 
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term:'' };
+
+        // take the existing function, bind it to "this" (instance of search bar), and replace the existing function with it. 
+        // If ever passing a callback around as a function like this and that callback has a reference to "this,
+        // you need to bind with the context. 
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    // all DOM event handlers (ie onChange, onClick, onHover, etc.) all come along with the event object.
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value});
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input 
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>

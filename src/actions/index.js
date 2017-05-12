91b08ea7eb89d025f6a1b5b4ca599288a2bac72e
jonathanlam <lamjwe91@gmail.com>
2017-05-12 00:05:21 -0700

    attempting to fix api call issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@
 // Doc: https://github.com/mzabriskie/axios
 import axios from 'axios';
 
-import APIKEY from './../../config'
 //const ROOT_URL = 'http://samples.openweathermap.org/data/2.5/forecast?appid=' + APIKEY;
 
 // new ES6 syntax - template string
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=0a0ed90de4f59e163e566368495daa7f`;
+const API_KEY = '0a0ed90de4f59e163e566368495daa7f'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${APIKEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
